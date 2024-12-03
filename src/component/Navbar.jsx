ad25708e19ca647afd022e5bd3824ccd88f23135
SheikBazith <83687604+SheikBazith@users.noreply.github.com>
2024-12-04 00:03:26 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className='nav-bar flex mt-6 place-content-between items-center'>
-        <div className='flex justify-center items-center gap-4'>
+       <NavLink to={'/'}><div className='flex justify-center items-center gap-4'>
             <img className='w-[63px]' src='images/pawsitivity 1.png'/>
             <h1 className='text-5xl'>Pawsitivity</h1>
-        </div>
+        </div></NavLink>
         <div>
             <ul className='flex hidden lg:flex gap-5 items-center'>
                 <NavLink to={"/"} ><li>Home</li></NavLink>
