ca8958b90122181cc1422205a5ed5b248389c073
SheikBazith <83687604+SheikBazith@users.noreply.github.com>
2025-01-12 19:05:25 +0530

    Update Homepage.jsx

--- src/component/Homepage.jsx
+++ src/component/Homepage.jsx
@@ -6,7 +6,7 @@ export default function Homepage() {
     <div className='home-page mt-6 flex flex-col md:flex-row justify-between'>
         <div className='flex flex-col justify-center items-center md:items-start mt-12 md:ml-6'>
             <h2 className='text-8xl lg:text-[180px] leading-[3rem] text-center md:text-left md:leading-[5.5rem]'>ADOPT,<br></br><span className='text-5xl md:text-8xl'>DON'T SHOP</span></h2>
-            <p className='text-center md:text-left'>Open your heart and home to an animal in need. <br></br>
+            <p className='text-center md:text-left'>Open your heart to an animal in need. <br></br>
             Bring home a forever friend.</p>
             <NavLink to="/adopt-now"><button className='adopt-btn mt-9 bg-black w-[100px]'>Adopt Now!</button></NavLink>
         </div>
