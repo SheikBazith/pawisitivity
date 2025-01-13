cb5f7d5b85a94b7634c8b3504dd230f3b2302806
SheikBazith <83687604+SheikBazith@users.noreply.github.com>
2025-01-13 10:21:19 +0530

    Update Newsletter.jsx

--- src/component/Newsletter.jsx
+++ src/component/Newsletter.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function Newsletter() {
   return (
-    <div className='newsletter bg-[#F6E6D6] flex flex-col lg:flex-row gap-9 justify-evenly items-center p-6 md:p-12 mt-6'>
+    <div className='bg-[#F6E6D6] flex flex-col lg:flex-row gap-9 justify-evenly items-center p-12 mt-6'>
 
         <div>
             <h4 className='text-5xl text-center'>Join our Newsletter</h4>
             <p className='text-center'>Sign up our newsletter for latest news</p>
         </div>
-        <div>
+        <div className='flex'>
             <input className='sub-place' placeholder='Enter Email Address' type='email'/>
             <button className='sub-btn'>Subscribe</button>
         </div>
